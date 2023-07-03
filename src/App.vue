72e53ca908c81fa1600593a2a070a93c998eed7d
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-03 14:22:16 +0200

    added store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,91 @@
-<script setup>
+<script>
+import axios from 'axios';
+import { store } from './data/store'
+export default {
+    data() {
+        return {
+            pokemons: store.pokemons
 
+        }
+    },
+    methods: {
+        getPokemons() {
+            axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?eq[type1]=Electric&sort[number]=desc').then(
+                (res) => {
+                    console.log(res.data)
+                    this.pokemons.push(res.data.docs)
+                }
+            )
+        }
+    },
+    created() {
+        this.getPokemons()
+    }
+}
 </script>
 
 <template>
-    <h1>CIAOOOOOO</h1>
+    <div class="container">
+        <main>
+            <div class="row">
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+                <div class="col-2">
+                    <h1>
+                        CIAOOOOO
+                    </h1>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-const axios = require('axios/dist/node/axios.cjs');
 createApp(App).mount('#app');
