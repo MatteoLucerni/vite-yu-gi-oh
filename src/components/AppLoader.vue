800b3d05455e5709c12063716ccce687b788d50d
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-04 22:14:56 +0200

    removed isEmpty

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -2,17 +2,12 @@
 export default {
     props: {
         isLoaded: Boolean,
-    },
-    data() {
-        return {
-            isEmpty: false
-        }
     }
 }
 </script>
 
 <template>
-    <h1 v-if="isLoaded === false && isEmpty === false" class="text-white">
+    <h1 v-if="isLoaded === false" class="text-white">
         <font-awesome-icon icon="fa-solid fa-bolt" />Carico...
     </h1>
 </template>
